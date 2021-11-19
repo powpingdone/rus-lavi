d9914510e24acdc03a17b93b9c2fec5010ff489f
Aidan Case <aidanzcase@gmail.com>
2021-11-18 23:30:56 -0600

    Added some rudementary argparsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+
+// Parse out all images that are not arguments
+fn parse_images(args: &mut Vec<String>) -> Vec<String> {
+    let mut imgs: Vec<String> = Vec::new();
+    let mut rems: Vec<usize> = Vec::new();
+    let mut hit_double_hyphen = false;
+
+    // get all the images
+    for (x, arg) in args.iter().enumerate() {
+        if hit_double_hyphen || arg.chars().next().unwrap() != '-' {
+            imgs.push(arg.to_string());
+            rems.push(x);
+        }
+        if arg == "--" {
+            hit_double_hyphen = true;
+        }
+    }
+
+    // remove the parsed images from the arg parser
+    rems.iter().fold(0, |accum, x| {
+        args.remove(x - accum);
+        accum + 1
+    });
+    return imgs;
+}
+
 fn main() {
-    println!("Hello, world!");
+    // parse the program args
+    let mut args: Vec<String> = std::env::args().collect();
+    args.remove(0); // skip the program name
+    let imgs: Vec<String> = parse_images(&mut args);
 }
