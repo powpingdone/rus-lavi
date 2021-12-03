553298d2f30ac5ecb27b3e2b12ffa6c24574423e
Aidan Case <aidanzcase@gmail.com>
2021-12-03 13:15:15 -0600

    something?

--- src/deviant.rs
+++ src/deviant.rs
@@ -1,7 +1,5 @@
 // deviant.rs: the main logic for finding the least average image
 
-mod imgload;
-
 pub fn least_average_arr(imgs: Vec<String>, res: (u32, u32)) -> Vec<f64> {
     let x: Vec<f64> = Vec::new();
     x
