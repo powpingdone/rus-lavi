291d5b4d1c990221d47c69fa2ae7de54ea427683
Aidan Case <aidanzcase@gmail.com>
2021-12-12 01:16:01 -0600

    Use `parking_lot`'s mutex
    This mutex does not eat memory, only adding 1 byte per pixel

--- src/deviant.rs
+++ src/deviant.rs
@@ -3,8 +3,9 @@
 use crate::imgload;
 use itertools::multizip;
 use linya::{Bar, Progress};
-use std::sync::{Arc, Mutex};
+use std::sync::Arc;
 use std::collections::VecDeque;
+use parking_lot::Mutex;
 
 // computes the least average image
 pub fn least_average_img(imgs: Vec<String>, res: (u32, u32), largest: String, verbose: bool) -> Vec<(u8, u8, u8)> {
@@ -13,13 +14,13 @@ pub fn least_average_img(imgs: Vec<String>, res: (u32, u32), largest: String, ve
     let img_main_master: Arc<Vec<Mutex<(u8, u8, u8)>>> = Arc::new(
         {
             let img = imgload::open_image(&largest, &res, verbose.clone()).unwrap();
-            img.iter().map(|x| Mutex::new(x.clone())).collect()
+            img.iter().map(|x| Mutex::new(*x)).collect()
         }
     );
 
     let mut dists_tmp: Vec<Mutex<f32>> = Vec::new();
     for pix in img_main_master.clone().iter() {
-        dists_tmp.push(Mutex::new(dist(&pix.lock().unwrap(), &(127, 127, 127))));
+        dists_tmp.push(Mutex::new(dist(&pix.lock(), &(127, 127, 127))));
     }
     let dists_master: Arc<Vec<Mutex<f32>>> = Arc::new(dists_tmp);
 
@@ -28,8 +29,8 @@ pub fn least_average_img(imgs: Vec<String>, res: (u32, u32), largest: String, ve
     let img_queue_master = Arc::new(Mutex::new(VecDeque::from(imgs)));
     let prog = Arc::new(Mutex::new(Progress::new()));
     let bar: Arc<Bar> = Arc::new(
-        prog.clone().lock().unwrap()
-        .bar(img_queue_master.clone().lock().unwrap().len(), "least averaging images")
+        prog.clone().lock()
+        .bar(img_queue_master.clone().lock().len(), "least averaging images")
     );
 
     // run threads
@@ -40,19 +41,19 @@ pub fn least_average_img(imgs: Vec<String>, res: (u32, u32), largest: String, ve
         let dists_master = dists_master.clone();
         let self_bar = bar.clone();
         let self_prog = prog.clone();
-        let largest_img = largest.clone(); // this clones the string, it is not atomic
+        let largest_img = largest.clone(); // this clones the string, it is not Arc
 
         // start thread
         thread_pool.push(std::thread::spawn(move || {
             loop {
-                let poss_img = { img_queue.lock().unwrap().pop_front() };
+                let poss_img = { img_queue.lock().pop_front() };
                 if let Some(img) = poss_img {
                     if img != largest_img {
                         if let Ok(img_loaded) = imgload::open_image(&img, &res, verbose.clone()) {
                             least_avg_thread(&img_main, &dists_master, img_loaded);
                         }
                     }
-                    self_prog.lock().unwrap().inc_and_draw(&self_bar, 1);
+                    self_prog.lock().inc_and_draw(&self_bar, 1);
                 }
                 else {
                     break;
@@ -67,14 +68,14 @@ pub fn least_average_img(imgs: Vec<String>, res: (u32, u32), largest: String, ve
     }
 
     // return non mutexed vec
-    Arc::clone(&img_main_master).iter().map(|x| x.lock().unwrap().clone()).collect()
+    Arc::clone(&img_main_master).iter().map(|x| x.lock().clone()).collect()
 }
 
 // thread function
 fn least_avg_thread(img_main: &Vec<Mutex<(u8, u8, u8)>>, dists: &Vec<Mutex<f32>>, img: Vec<(u8, u8, u8)>) {
     for (base, merge, old_dist) in multizip((img_main.iter(), img.iter(), dists.iter())) {
-        let mut old_dist = old_dist.lock().unwrap();
-        let mut base = base.lock().unwrap();
+        let mut old_dist = old_dist.lock();
+        let mut base = base.lock();
         let new_dist = dist(&merge, &base);
 
         // update references
